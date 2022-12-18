cf7dd6322811bfcc07f4dfbd8167c85886ebfb12
vasyllukian <91331245+vasyllukian@users.noreply.github.com>
2022-12-18 21:53:53 +0200

    Add files via upload

---
+++ lab2/lab2.py
@@ -0,0 +1,19 @@
+from matplotlib import pyplot as plt
+import numpy as np
+fig, ax = plt.subplots()
+f = open("D:\\the work\sem3 exclusive\comp graphics and multimedia\lab2\DS7.txt")
+xs = []
+ys = []
+for line in f:
+    y,x = line.split(" ")
+    xs.append(x)
+    ys.append(y)
+f.close()
+npxs = np.array(xs, 'int16') 
+npys = np.array(ys, 'int16')
+plt.xlim(0,960)
+plt.ylim(0,540)
+lines = plt.plot(npxs,npys)
+plt.setp(lines, linestyle = '') 
+ax.scatter(npxs, npys, s=1)
+plt.show()
